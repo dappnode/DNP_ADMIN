bb2f7f89840a686fa2e321bfb144e247f55ff410
Unknown <guillem.lion.benosa@gmail.com>
2018-05-26 12:07:29 +0200

    fixed home page

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,4 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
+import { MdDashboard, MdDevices, MdSettingsApplications,
+    MdAddBox, MdCreateNewFolder, MdFolder,
+  MdSync } from 'react-icons/lib/md'
+
+let navbarItemsInfo = [
+  {
+    name: 'Dashboard',
+    href: '/dashboard',
+    icon: MdDashboard
+  },
+  {
+    name: 'Devices',
+    href: '/devices',
+    icon: MdDevices
+  },
+  {
+    name: 'Installer',
+    href: '/installer',
+    icon: MdCreateNewFolder
+  },
+  {
+    name: 'Packages',
+    href: '/packages',
+    icon: MdFolder
+  }
+]
 
 export default class Home extends React.Component {
   constructor() {
@@ -6,9 +33,37 @@ export default class Home extends React.Component {
   }
 
   render() {
+
+    const items = navbarItemsInfo.map((item, i) => {
+      return (
+        <div key={i} class="col">
+          <NavLink
+            class="nav-link"
+            to={item.href}
+            >
+            <button type="button" class="btn btn-outline-dark btn-lg btn-block">
+              <div class="nav-link-icon"><item.icon size={60}/></div>
+              <div>{item.name}</div>
+            </button>
+          </NavLink>
+        </div>
+      )
+    })
+
     return (
       <div class='body'>
-        <h1>DappNode home</h1>
+
+        <div class="jumbotron">
+        <h1 class="display-4">Welcome to DAppNode</h1>
+        <p class="lead">If you have just finished the installation, please help the team telling us how it went in the survey below</p>
+        <p class="lead">
+          <a class="btn dappnode-background-color btn-lg" href="#" role="button">Fill survey</a>
+        </p>
+        </div>
+
+        <div class="row mt-4">
+          {items}
+        </div>
       </div>
     );
   }
