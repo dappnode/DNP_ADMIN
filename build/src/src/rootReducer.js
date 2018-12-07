8d8d7f9a08fa86c3a95b725503580332951265f8
Unknown <guillem.lion.benosa@gmail.com>
2018-12-07 18:50:56 +0800

    Ping dependant DNP from the API

--- build/src/src/rootReducer.js
+++ build/src/src/rootReducer.js
@@ -64,6 +64,17 @@ const globalReducers = {
       default:
         return state;
     }
+  },
+  packageStatus: (state = {}, action) => {
+    switch (action.type) {
+      case "UPDATE_PACKAGE_STATUS":
+        return {
+          ...state,
+          [action.packageName]: action.connected
+        }
+      default:
+        return state;
+    }
   }
 }
 
