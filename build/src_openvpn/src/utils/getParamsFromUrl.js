d1dd80370d54683d27ee87176e15b44285fe6411
Unknown <guillem.lion.benosa@gmail.com>
2019-01-19 15:05:51 +0700

    only return index.html to users with a valid id

--- build/src_openvpn/src/utils/getParamsFromUrl.js
+++ build/src_openvpn/src/utils/getParamsFromUrl.js
@@ -5,15 +5,18 @@
 // 
 // http://origin/7e00cfadbe61f2ed#mc5pGQQ4VbbuWJDayJD0kXsElAUddmUktJYUYSDNaDE=
 
+const urlTemplate = 'http://origin/?id=<id>#<key>'
+
 export default function getParamsFromUrl() {
-  const id = clean(window.location.pathname)
+  const idArg = clean(window.location.search).split('&').find(arg => arg.startsWith('id='))
+  const id = (idArg || '').split('id=')[1]
   const key = clean(window.location.hash)
-  if (!id) throw Error('No valid id provided. Url must be http://origin/id#key')
-  if (!key) throw Error('No valid key provided. Url must be http://origin/id#key')
+  if (!id) throw Error('No valid id provided. Url must be '+urlTemplate)
+  if (!key) throw Error('No valid key provided. Url must be '+urlTemplate)
   return { key, id };
 }
 
 function clean(s) {
   if (!s) return s
-  return s.trim().replace('/', '').replace('#', '')
+  return s.trim().replace('/', '').replace('#', '').replace('?', '')
 }
