eab367fed6742cd0f8e182599d1b35b02400cc8d
Unknown <guillem.lion.benosa@gmail.com>
2019-02-07 15:10:05 +0100

    Format userSet objects for install call

--- build/src/src/installer/components/InstallCardComponents/ApproveInstall.jsx
+++ build/src/src/installer/components/InstallCardComponents/ApproveInstall.jsx
@@ -4,7 +4,6 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import * as action from "../../actions";
 import * as selector from "../../selectors";
-import * as utils from "../../utils";
 // Components
 import SpecialPermissions from "./SpecialPermissions";
 import Envs from "./Envs";
@@ -14,49 +13,17 @@ import Dependencies from "./Dependencies";
 import Details from "./Details";
 import { Link } from "react-router-dom";
 import packages from "packages";
-import parsePorts from "utils/parsePorts";
 // style
 import "./checkbox.css";
-
-/**
- * Parses envs
- * @param {object} manifest
- * @return {object} {envName: 'envValue'}
- */
-
-function getEnvs(manifest, stateEnv) {
-  const envsArray = ((manifest || {}).image || {}).environment || [];
-  const defaultEnvs = {};
-  for (const row of envsArray) {
-    defaultEnvs[row.split("=")[0]] = row.split("=")[1] || "";
-  }
-  const _stateEnv = Object.assign({}, stateEnv);
-  // Verify that the current stateEnv contains only this package's envs
-  for (const env of Object.getOwnPropertyNames(_stateEnv)) {
-    if (!Object.keys(defaultEnvs).includes(env)) {
-      delete _stateEnv[env];
-    }
-  }
-  return {
-    ...defaultEnvs,
-    ..._stateEnv
-  };
-}
 
 class ApproveInstallView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      envs: {},
-      userSetVols: {},
-      userSetPorts: {},
       options: {
         BYPASS_CORE_RESTRICTION: false
       }
     };
-    this.handleEnvChange = this.handleEnvChange.bind(this);
-    this.handleVolChange = this.handleVolChange.bind(this);
-    this.handlePortChange = this.handlePortChange.bind(this);
     this.handleOptionChange = this.handleOptionChange.bind(this);
     this.approveInstall = this.approveInstall.bind(this);
   }
@@ -67,33 +34,6 @@ class ApproveInstallView extends React.Component {
     request: PropTypes.object.isRequired,
     install: PropTypes.func.isRequired
   };
-
-  handleEnvChange({ value, name }) {
-    this.setState({
-      envs: {
-        ...this.state.envs,
-        [name]: value
-      }
-    });
-  }
-
-  handleVolChange({ newVol, vol }) {
-    this.setState({
-      userSetVols: {
-        ...this.state.userSetVols,
-        [vol]: newVol
-      }
-    });
-  }
-
-  handlePortChange({ newPort, port }) {
-    this.setState({
-      userSetPorts: {
-        ...this.state.userSetPorts,
-        [port]: newPort
-      }
-    });
-  }
 
   handleOptionChange(e) {
     const { name, checked } = e.target;
@@ -106,65 +46,18 @@ class ApproveInstallView extends React.Component {
   }
 
   approveInstall() {
-    const manifest = this.props.manifest || {};
-    // Get envs
-    const envs = getEnvs(manifest, this.state.envs);
-
-    // Merge ports and parse them
-    const portsArray = ((manifest.image || {}).ports || []).map(
-      port => this.state.userSetPorts[port] || port
-    );
-    // IN: ['32323:30303/udp', '30304'], OUT: [{number:'32323', type:'UDP'}]
-    const ports = parsePorts(portsArray);
-
     // Call install
     // Path ipfs names:
     let id = this.props.id;
-    let { name, version } = manifest;
+    let { name, version } = this.props.manifest || {};
     if (id.startsWith("/ipfs/")) id = name + "@" + id;
     else id = name + "@" + version;
 
-    // ##### The by package notation is a forward compatibility
-    // ##### to suppport setting dependencies' port / vol
-    //  userSetVols = {
-    //    "kovan.dnp.dappnode.eth": {
-    //      "old_path:/root/.local": "new_path:/root/.local"
-    //    }, ... }
-    //  userSetPorts = {
-    //    "kovan.dnp.dappnode.eth": {
-    //      "30303": "31313:30303",
-    //      "30303/udp": "31313:30303/udp"
-    //    }, ... }
-    const userSetVols = {
-      [name]: this.state.userSetVols
-    };
-    const userSetPorts = {
-      [name]: this.state.userSetPorts
-    };
-    const userSetEnvs = {};
-
     // Fire install call
-    const options = this.state.options;
-    this.props.install({ id, userSetEnvs, userSetVols, userSetPorts, options });
-    // Fire call to set dependency envs
-    if (manifest && manifest.dependencies) {
-      for (const depName of Object.keys(manifest.dependencies)) {
-        const depVersion = manifest.dependencies[depName];
-        if (utils.isIpfsHash(depVersion)) {
-          this.props.updateDefaultEnvs({ id: depVersion });
-        } else {
-          this.props.updateDefaultEnvs({ id: depName });
-        }
-      }
-    }
+    this.props.install({ id, options: this.state.options });
   }
 
   render() {
-    const manifest = this.props.manifest || {};
-    // const envs = getEnvs(manifest, this.state.envs);
-    const manifestVols = (manifest.image || {}).volumes || [];
-    const manifestPorts = (manifest.image || {}).ports || [];
-
     // Get install tag: INSTALL / UPDATE / INSTALLED
     let tag = this.props.pkg.tag || "INSTALL";
 
