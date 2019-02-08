cc58af7081141329dc84d2130dc17843078e1cc3
Unknown <guillem.lion.benosa@gmail.com>
2019-02-08 13:10:57 +0100

    Show shoulder surfing warning

--- build/src/src/devices/components/DeviceSettings.jsx
+++ build/src/src/devices/components/DeviceSettings.jsx
@@ -34,37 +34,43 @@ class DevicesSettings extends React.Component {
         </div>
         <div className="card mb-3" id={id}>
           <div className="card-body" style={{ padding }}>
-            <div>
-              <div className="float-left" style={{ margin, width: "200px" }}>
+            <div className="d-flex justify-content-between">
+              <div style={{ margin, width: "200px" }}>
                 <h5 className="card-title">{id}</h5>
                 <p className="card-text">
                   {ipField && <span className="ipBadge">{ipField}</span>}
                   {isAdmin && <span className="adminBadge">ADMIN</span>}
                 </p>
               </div>
-              <div>
-                <div
-                  className="btn-group float-right"
-                  role="group"
-                  style={{ margin }}
-                >
-                  <NavLink to={"/devices/"}>
-                    <button type="button" className="btn btn-outline-secondary">
-                      Back
-                    </button>
-                  </NavLink>
-                </div>
+              <div className="btn-group" role="group" style={{ margin }}>
+                <NavLink to={"/devices/"}>
+                  <button type="button" className="btn btn-outline-secondary">
+                    Back
+                  </button>
+                </NavLink>
               </div>
             </div>
 
-            <div style={{ maxWidth: "400px", margin }}>
-              {url && (
-                <QRCode
-                  value={url}
-                  renderAs="svg"
-                  style={{ width: "100%", height: "100%" }}
-                />
-              )}
+            <div className="d-flex justify-content-center">
+              <div className="mt-3" style={{ margin }}>
+                {url && (
+                  <div className="alert alert-warning" role="alert">
+                    Beware of shoulder surfing attacks (unsolicited observers),
+                    This QR code will grant them access to your DAppNode
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="d-flex justify-content-center">
+              <div style={{ maxWidth: "400px", margin }}>
+                {url && (
+                  <QRCode
+                    value={url}
+                    renderAs="svg"
+                    style={{ width: "100%", height: "100%" }}
+                  />
+                )}
+              </div>
             </div>
           </div>
         </div>
