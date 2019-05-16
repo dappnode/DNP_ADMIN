e598a2bab23c977b5c1a892b3441425f7699626e
dapplion <guillem.lion.benosa@gmail.com>
2019-05-16 11:32:31 +0200

    Simplify the device credentials flow

--- build/src/src/pages/devices/actions.js
+++ build/src/src/pages/devices/actions.js
@@ -1,6 +1,8 @@
 // DEVICES
 import api from "API/rpcMethods";
 import { updateDevice } from "services/devices/actions";
+import { superAdminId } from "services/devices/data";
+import { confirm } from "components/ConfirmDialog";
 
 /* Notice: addDevice, removeDevice, etc use redux-thunk
    Since there is no return value, and the state change
@@ -17,20 +19,33 @@ export const addDevice = id => () =>
   api.addDevice({ id }, { toastMessage: `Adding ${id}...` });
 
 export const removeDevice = id => () =>
-  api.removeDevice({ id }, { toastMessage: `Removing ${id}...` });
+  confirm({
+    title: `Removing ${id} device`,
+    text: "The user using this device will lose access to this DAppNode ",
+    label: "Remove",
+    onClick: () =>
+      api.removeDevice({ id }, { toastMessage: `Removing ${id}...` })
+  });
 
-export const resetDevice = id => () =>
-  api.resetDevice({ id }, { toastMessage: `Reseting ${id}...` });
+export const resetDevice = id => () => {
+  const isSuperAdmin = id === superAdminId;
+  confirm({
+    title: isSuperAdmin
+      ? `WARNING! Reseting super admin`
+      : `Reseting ${id} device`,
+    text: isSuperAdmin
+      ? "You should only reset the credentials of the super admin if you suspect an unwanted party gained access to this credentials. If that is the case, reset the credentials, BUT download and install the new credentials IMMEDIATELY. Otherwise, you will lose access to your DAppNode when this connection stops"
+      : "All profiles and links pointing to this device will no longer be valid",
+    label: `Reset`,
+    onClick: () =>
+      api.resetDevice({ id }, { toastMessage: `Reseting ${id}...` })
+  });
+};
 
 export const toggleAdmin = id => () =>
   api.toggleAdmin({ id }, { toastMessage: `Toggling ${id} admin...` });
 
 export const getDeviceCredentials = id => dispatch =>
-  api
-    .getDeviceCredentials(
-      { id },
-      { toastMessage: `Getting ${id} credentials...` }
-    )
-    .then(data => {
-      dispatch(updateDevice(id, data));
-    });
+  api.getDeviceCredentials({ id }, { toastOnError: true }).then(data => {
+    dispatch(updateDevice(id, data));
+  });
--- build/src/src/pages/devices/components/DeviceGrid.jsx
+++ build/src/src/pages/devices/components/DeviceGrid.jsx
@@ -1,64 +1,16 @@
-import React, { useEffect } from "react";
-import ClipboardJS from "clipboard";
+import React from "react";
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
-import { superAdminId } from "services/devices/data";
 // Components
 import Card from "components/Card";
 import Switch from "components/Switch";
-import { confirm } from "components/ConfirmDialog";
+import { ButtonLight } from "components/Button";
 // Helpers
 // import downloadVpnCredentials from "../helpers/downloadVpnCredentials";
-// Utils
-import newTabProps from "utils/newTabProps";
 // Icons
-import { MdDelete, MdRefresh, MdShare } from "react-icons/md";
-import { FaQrcode, FaDownload } from "react-icons/fa";
-import { GoClippy } from "react-icons/go";
-import "./devices.css";
+import { MdDelete, MdRefresh } from "react-icons/md";
 
-function DeviceGrid({
-  devices,
-  removeDevice,
-  resetDevice,
-  toggleAdmin,
-  getDeviceCredentials
-}) {
-  // Activate the copy functionality
-  useEffect(() => {
-    new ClipboardJS(".copy");
-  }, []);
-
-  function removeDeviceConfirm(id) {
-    confirm({
-      title: `Removing ${id} device`,
-      text: "The user using this device will lose access to this DAppNode ",
-      label: "Remove",
-      onClick: () => removeDevice(id)
-    });
-  }
-
-  function resetDeviceConfirm(id) {
-    const numOfAdmins = devices.filter(({ admin }) => admin).length;
-    if (id === superAdminId || numOfAdmins === 1) {
-      confirm({
-        title: `WARNING! Reseting super admin`,
-        text:
-          "You should only reset the credentials of the super admin if you suspect an unwanted party gained access to this credentials. If that is the case, reset the credentials, BUT download and install the new credentials IMMEDIATELY. Otherwise, you will lose access to your DAppNode when this connection stops",
-        label: `Reset ${id}`,
-        onClick: () => resetDevice(id)
-      });
-    } else {
-      confirm({
-        title: `Reseting ${id} device`,
-        text:
-          "All profiles and links pointing to this device will no longer be valid",
-        label: "Reset",
-        onClick: () => resetDevice(id)
-      });
-    }
-  }
-
+function DeviceGrid({ devices, removeDevice, resetDevice, toggleAdmin }) {
   // function download(id) {
   //   const device = devices.find(d => d.id === id);
   //   if (device) downloadVpnCredentials(device);
@@ -67,31 +19,22 @@ function DeviceGrid({
   return (
     <Card className="list-grid devices">
       <header>Name</header>
-      <header className="center">Share</header>
+      <header className="center">Credentials</header>
       <header>Admin</header>
       <header>Reset</header>
       <header>Remove</header>
       {devices.map(({ id, admin, url }) => (
         <React.Fragment key={id}>
           <div className="name">{id}</div>
-          {url ? (
-            <div className="group no-a-style">
-              <NavLink to={"/devices/" + id}>
-                <FaQrcode className="bigger" />
-              </NavLink>
-              <GoClippy className="copy" data-clipboard-text={url} />
-              <a href={url} {...newTabProps}>
-                <FaDownload className="smaller" />
-              </a>
-            </div>
-          ) : (
-            <MdShare onClick={() => getDeviceCredentials(id)} />
-          )}
+          <NavLink to={"/devices/" + id} className="no-a-style">
+            <ButtonLight className="get-link">Get</ButtonLight>
+          </NavLink>
+
           <Switch checked={admin} onToggle={() => toggleAdmin(id)} />
-          <MdRefresh onClick={() => resetDeviceConfirm(id)} />
+          <MdRefresh onClick={() => resetDevice(id)} />
           <MdDelete
             className={admin ? "disabled" : ""}
-            onClick={() => (admin ? null : removeDeviceConfirm(id))}
+            onClick={() => (admin ? null : removeDevice(id))}
           />
           <hr />
         </React.Fragment>
@@ -110,8 +53,7 @@ DeviceGrid.propTypes = {
   ).isRequired,
   removeDevice: PropTypes.func.isRequired,
   resetDevice: PropTypes.func.isRequired,
-  toggleAdmin: PropTypes.func.isRequired,
-  getDeviceCredentials: PropTypes.func.isRequired
+  toggleAdmin: PropTypes.func.isRequired
 };
 
 export default DeviceGrid;
