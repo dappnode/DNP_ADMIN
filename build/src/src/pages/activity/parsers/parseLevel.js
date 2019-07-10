b640ea5a784ba2544dacd58b8702d05ebc968a63
dapplion <guillem.lion.benosa@gmail.com>
2019-07-10 12:13:20 +0200

    Join report and activity under Support
    del

--- build/src/src/pages/activity/parsers/parseLevel.js
+++
@@ -1,5 +0,0 @@
-export default function parseLevel(level) {
-  if (level === "error") return "danger";
-  if (level === "warn") return "warning";
-  if (level === "info") return "success";
-}
--- build/src/src/pages/index.js
+++ build/src/src/pages/index.js
@@ -1,21 +1,19 @@
-import activity from "./activity";
 import dashboard from "./dashboard";
 import devices from "./devices";
 import home from "./home";
 import installer from "./installer";
 import packages from "./packages";
 import sdk from "./sdk";
+import support from "./support";
 import system from "./system";
-import troubleshoot from "./troubleshoot";
 
 export default {
-  activity,
   dashboard,
   devices,
   home,
   installer,
   packages,
   sdk,
-  system,
-  troubleshoot
+  support,
+  system
 };
