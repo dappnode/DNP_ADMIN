81cdb481a3f5794478778a37cde3492627592833
dapplion <guillem.lion.benosa@gmail.com>
2019-11-15 11:26:29 +0100

    Update volume info data types

--- build/src/src/pages/packages/components/PackageViews/Info/Vols.jsx
+++ build/src/src/pages/packages/components/PackageViews/Info/Vols.jsx
@@ -15,15 +15,17 @@ function Vols({ dnp }) {
       title={"Volumes"}
       data={(volumes || [])
         // Order volumes before bind mounts
-        .sort(v1 => (v1.type === "volume" ? -1 : 1))
+        .sort(v1 => (v1.name ? -1 : 1))
         // Order volumes with a bigger size first
         .sort(v1 => ((v1.name || "").includes("data") ? -1 : 0))
         // Display style:
         // - dncore_vpndnpdappnodeeth_data: 866B
         // - /etc/hostname: - (bind)
-        .map(({ name, path, size, type }) => ({
-          name: name ? prettyVolumeName(name, dnp.name) : path || "Unknown",
-          size: size ? prettyBytes(size) : type === "bind" ? "(bind)" : "..."
+        .map(({ name, container, size }) => ({
+          name: name
+            ? prettyVolumeName(name, dnp.name)
+            : container || "Unknown",
+          size: size ? prettyBytes(size) : !name ? "(bind)" : "..."
         }))
         .map(({ name, size }) => (
           <>
@@ -34,26 +36,12 @@ function Vols({ dnp }) {
   );
 }
 
-/**
- * VOLUMES
- * dnp.volumes = [
- *   { type: "bind",
- *     path: "/etc/hostname" },
- *   { type: "volume",
- *     name: "dncore_ethchaindnpdappnodeeth_data",
- *     path: "/var/lib/docker/volumes/dncore_ethchaindnpdappnodeeth_data/_data",
- *     links: "1",
- *     size: 45134817123}
- * ]
- */
-
 Vols.propTypes = {
   dnp: PropTypes.shape({
     volumes: PropTypes.arrayOf(
       PropTypes.shape({
-        type: PropTypes.string.isRequired,
-        path: PropTypes.string.isRequired,
         name: PropTypes.string,
+        container: PropTypes.string,
         size: PropTypes.number
       })
     ).isRequired
