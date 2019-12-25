7f72676a4090b71d795f180799877b9fe57f8808
dapplion <guillem.lion.benosa@gmail.com>
2019-12-25 02:34:03 +0100

    Simplify route imports

--- build/src/src/API/calls.ts
+++ build/src/src/API/calls.ts
@@ -1,50 +1,13 @@
 import { wrapRoute } from "./wrapRoute";
+import * as routes from "../route-types";
 
-import * as _fetchCoreUpdateData from "../route-types/fetchCoreUpdateData";
-import * as _fetchDirectory from "../route-types/fetchDirectory";
-import * as _fetchDnpRequest from "../route-types/fetchDnpRequest";
-import * as _installPackage from "../route-types/installPackage";
-import * as _packageDetailDataGet from "../route-types/packageDetailDataGet";
-import * as _mountpointsGet from "../route-types/mountpointsGet";
-import * as _listPackages from "../route-types/listPackages";
-import * as _packageGettingStartedToggle from "../route-types/packageGettingStartedToggle";
-
-export const fetchCoreUpdateData = wrapRoute<
-  _fetchCoreUpdateData.RequestData,
-  _fetchCoreUpdateData.ReturnData
->(_fetchCoreUpdateData);
-
-export const fetchDirectory = wrapRoute<
-  _fetchDirectory.RequestData,
-  _fetchDirectory.ReturnData
->(_fetchDirectory);
-
-export const fetchDnpRequest = wrapRoute<
-  _fetchDnpRequest.RequestData,
-  _fetchDnpRequest.ReturnData
->(_fetchDnpRequest);
-
-export const installPackage = wrapRoute<
-  _installPackage.RequestData,
-  _installPackage.ReturnData
->(_installPackage);
-
-export const mountpointsGet = wrapRoute<
-  _mountpointsGet.RequestData,
-  _mountpointsGet.ReturnData
->(_mountpointsGet);
-
-export const packageDetailDataGet = wrapRoute<
-  _packageDetailDataGet.RequestData,
-  _packageDetailDataGet.ReturnData
->(_packageDetailDataGet);
-
-export const listPackages = wrapRoute<
-  _listPackages.RequestData,
-  _listPackages.ReturnData
->(_listPackages);
-
-export const packageGettingStartedToggle = wrapRoute<
-  _packageGettingStartedToggle.RequestData,
-  _packageGettingStartedToggle.ReturnData
->(_packageGettingStartedToggle);
+export const fetchCoreUpdateData = wrapRoute(routes.fetchCoreUpdateData);
+export const fetchDirectory = wrapRoute(routes.fetchDirectory);
+export const fetchDnpRequest = wrapRoute(routes.fetchDnpRequest);
+export const installPackage = wrapRoute(routes.installPackage);
+export const mountpointsGet = wrapRoute(routes.mountpointsGet);
+export const packageDetailDataGet = wrapRoute(routes.packageDetailDataGet);
+export const listPackages = wrapRoute(routes.listPackages);
+export const packageGettingStartedToggle = wrapRoute(
+  routes.packageGettingStartedToggle
+);
---
+++ build/src/src/route-types/index.ts
@@ -0,0 +1,19 @@
+import * as fetchCoreUpdateData from "./fetchCoreUpdateData";
+import * as fetchDirectory from "./fetchDirectory";
+import * as fetchDnpRequest from "./fetchDnpRequest";
+import * as installPackage from "./installPackage";
+import * as listPackages from "./listPackages";
+import * as mountpointsGet from "./mountpointsGet";
+import * as packageDetailDataGet from "./packageDetailDataGet";
+import * as packageGettingStartedToggle from "./packageGettingStartedToggle";
+
+export {
+  fetchCoreUpdateData,
+  fetchDirectory,
+  fetchDnpRequest,
+  installPackage,
+  listPackages,
+  mountpointsGet,
+  packageDetailDataGet,
+  packageGettingStartedToggle
+};
