7f29d5a25f742ff491ee449852043c4a8c6b9a3a
dapplion <guillem.lion.benosa@gmail.com>
2020-05-04 00:08:14 +0200

    Remove chainData saga

--- build/src/src/rootSaga.ts
+++ build/src/src/rootSaga.ts
@@ -1,10 +1,9 @@
 import { all } from "redux-saga/effects";
 
-import chainDataSaga from "services/chainData/sagas";
 import dappnodeStatusSaga from "services/dappnodeStatus/sagas";
 
 // notice how we now only export the rootSaga
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
-  yield all([chainDataSaga(), dappnodeStatusSaga()]);
+  yield all([dappnodeStatusSaga()]);
 }
