02cc130b04ad88d98e4e666b0edc7a65c1ff24da
dapplion <guillem.lion.benosa@gmail.com>
2020-05-04 00:36:05 +0200

    Remove dappnodeStatus saga

--- build/src/src/rootSaga.ts
+++
@@ -1,9 +0,0 @@
-import { all } from "redux-saga/effects";
-
-import dappnodeStatusSaga from "services/dappnodeStatus/sagas";
-
-// notice how we now only export the rootSaga
-// single entry point to start all Sagas at once
-export default function* rootSaga() {
-  yield all([dappnodeStatusSaga()]);
-}
